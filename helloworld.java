9378320ebd5e109e0aa20fb639ec1405301076da
shaswattrivedi <shaswattrivedi@gmail.com>
2026-01-20 01:36:44 +0530

    Hello World

--- helloworld.java
+++ helloworld.java
@@ -3,3 +3,8 @@ class Hello {
         System.out.println("hello world");
     }
 }
+
+//Write a program to add 2 numbers
+//Write a program to multiply 2 numbers 
+//Write a program to print my name
+//Write a program to print my best friend's name
