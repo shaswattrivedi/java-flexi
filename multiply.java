657c36779396bd5ed45b0e061127957498af5d63
shaswattrivedi <shaswattrivedi@gmail.com>
2026-01-20 01:28:41 +0530

    Product of 2 numbers

---
+++ multiply.java
@@ -0,0 +1,8 @@
+class Multiply{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int mul = a*b;
+        System.out.println("Product of 2 numbers -> " + mul);
+    }
+}
