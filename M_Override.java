647800a197b81d284948940aab9bad843e77cfaf
shaswattrivedi <shaswattriv@gmail.com>
2026-02-03 19:52:26 +0530

    Method Overriding Program

---
+++ M_Override.java
@@ -0,0 +1,28 @@
+class BankAccount {
+    double getInterestRate() {
+        return 0.0;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    @Override
+    double getInterestRate() {
+        return 4.5;
+    }
+}
+
+class CurrentAccount extends BankAccount {
+    @Override
+    double getInterestRate() {
+        return 3.0;
+    }
+}
+
+public class M_Override {
+    public static void main(String[] args) {
+        BankAccount sa = new SavingsAccount();
+        BankAccount ca = new CurrentAccount();
+        System.out.println("Savings Interest Rate: " + sa.getInterestRate());
+        System.out.println("Current Interest Rate: " + ca.getInterestRate());
+    }
+}
