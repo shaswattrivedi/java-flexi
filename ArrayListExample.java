d9f7bd0a076fec15311629dcde741af223b83658
shaswattrivedi <shaswattriv@gmail.com>
2026-02-07 15:05:10 +0530

    Array List Program

---
+++ ArrayListExample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        System.out.println("Fruits " + fruits);
+
+        fruits.remove("Banana");
+        System.out.println("Fruits " + fruits);
+        System.out.println("Size " + fruits.size());
+        System.out.println("Fruits have Apple?" + fruits.contains("Cherry"));
+    }
+}
